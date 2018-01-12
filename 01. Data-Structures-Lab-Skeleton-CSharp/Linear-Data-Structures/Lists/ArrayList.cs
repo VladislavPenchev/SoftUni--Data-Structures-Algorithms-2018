3d80061408021f43f34ca883d11d4f00b4f5e1c2
VladislavPenchev <vgpenchev@gmail.com>
2018-01-12 23:40:44 +0200

    List->Commit structure of ArrayList Csharp

---
+++ 01. Data-Structures-Lab-Skeleton-CSharp/Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private T[] arr;
+
+    public ArrayList(int capacity = 2)
+    {
+        this.arr = new T[Capacity]; //initialisation the array
+    }
+
+    public int Capacity { get; set; }
+
+    public int Count { get; set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
