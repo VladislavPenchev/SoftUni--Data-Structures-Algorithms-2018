3b88ba1800cacc750d8fd8630e3a97564ee627f1
VladislavPenchev <vgpenchev@gmail.com>
2018-01-15 13:53:42 +0200

    Implement LinkedList Add/Remove/GetEnumerate

--- 02. Linked-List-CSharp-Skeleton/02. Linked-List-CSharp-Skeleton/LinkedList/LinkedList.cs
+++ 02. Linked-List-CSharp-Skeleton/02. Linked-List-CSharp-Skeleton/LinkedList/LinkedList.cs
@@ -4,41 +4,139 @@ using System.Collections.Generic;
 
 public class LinkedList<T> : IEnumerable<T>
 {
+    private Node head;
+    private Node tail;
+
+    public LinkedList()
+    {
+        this.head = null;
+        this.tail = null;
+        this.Count = 0;
+
+    }
+
     public int Count { get; private set; }
 
     public void AddFirst(T item)
     {
-        // TODO
-        throw new NotImplementedException();
+        Node newNode = new Node(item);
+
+        if (this.Count == 0)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            newNode.Next = this.head;
+            this.head = newNode;
+        }
+
+        this.Count++;
+
     }
 
     public void AddLast(T item)
     {
-        // TODO
-        throw new NotImplementedException();
+        Node newNode = new Node(item);
+
+        if(this.Count == 0)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            this.tail.Next = newNode;
+            this.tail = newNode;
+        }
+        this.Count++;
     }
 
     public T RemoveFirst()
     {
-        // TODO: Throw exception if the list is empty
-        throw new NotImplementedException();
+        if(this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T element = this.head.Value;
+
+        if (this.Count == 1)
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else 
+        {
+            this.head = this.head.Next;
+        }
+
+        this.Count--;
+
+        return element;
     }
 
     public T RemoveLast()
     {
-        // TODO: Throw exception if the list is empty
-        throw new NotImplementedException();
+
+        if (this.Count == 0) 
+        {
+            throw new InvalidOperationException();
+        }
+
+        T element = this.tail.Value;
+
+        if (Count == 1) 
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else
+        {
+            //Parent
+            Node parent = this.head;
+            while (parent.Next != this.tail)
+            {
+                parent = parent.Next;
+            }
+
+            parent.Next = null;
+            this.tail = parent;
+        }
+        this.Count--;
+        
+        return element;
+
     }
 
     public IEnumerator<T> GetEnumerator()
     {
-        // TODO
-        throw new NotImplementedException();
+        Node start = this.head;
+        while(start != null)
+        {
+            yield return start.Value;
+            start = start.Next;
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        // TODO
-        throw new NotImplementedException();
+        return this.GetEnumerator();
     }
+
+    private class Node
+    {
+        public T Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+
 }
+
+
---
+++ Node/StartUp/StartUpp.cs
@@ -0,0 +1,87 @@
+﻿namespace StartUp
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUpp
+    {
+        static void Main()
+        {
+            List<Node<int>> nodeSaves = new List<Node<int>>();
+
+            Node<int> head = new Node<int>();
+            Node<int> node1 = new Node<int>();
+            Node<int> node2 = new Node<int>();
+
+            head.Next = node1;
+            head.Value = 1;
+            head.Name = "Head";
+
+            node1.Next = node2;
+            node1.Value = 2;
+            node1.Name = "Node1";
+
+            node2.Next = null;
+            node2.Value = 3;
+            node2.Name = "Node2";
+
+
+            nodeSaves.Add(head);
+            nodeSaves.Add(node1);
+            nodeSaves.Add(node2);
+
+            foreach (var node in nodeSaves)
+            {
+                if (node2.Next == null)
+                {
+                    //Console.WriteLine(head.Next);
+                    //Console.WriteLine(head.Value);
+
+                    //Console.WriteLine(node1.Next);
+                    //Console.WriteLine(node1.Value);
+
+                    //Console.WriteLine(node2.Next);
+                    //Console.WriteLine(node2.Value);
+
+                    Console.WriteLine("Name  -> {2}               REferens-Code ->{0},ValueOfNode -> {1}",node.Next,node.Value,node.Name);
+                }
+            }
+
+            head = head.Next;
+            head.Value = node1.Value;
+
+            nodeSaves.Remove(head);
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            foreach (var node in nodeSaves)
+            {
+                if (node2.Next == null)
+                {
+                    //Console.WriteLine(head.Next);
+                    //Console.WriteLine(head.Value);
+
+                    //Console.WriteLine(node1.Next);
+                    //Console.WriteLine(node1.Value);
+
+                    //Console.WriteLine(node2.Next);
+                    //Console.WriteLine(node2.Value);
+                    Console.WriteLine("Name  -> {2}               REferens-Code ->{0},ValueOfNode -> {1}", node.Next, node.Value, node.Name);
+
+                }
+            }
+
+
+            if (nodeSaves.Count == 2)
+            {
+
+            }
+        }
+    }
+}
+
+class Node<T>
+{
+    public string Name { get; set; }
+    public T Value { get; set; }
+
+    public Node<T> Next { get; set; }
+}
