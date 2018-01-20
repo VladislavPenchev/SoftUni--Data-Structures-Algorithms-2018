1bc8fa36d587761452db7acdb97956280d88aede
VladislavPenchev <vgpenchev@gmail.com>
2018-01-20 21:51:40 +0200

    full implement sort words

--- 3.ExersiceHW1/ConsoleApp1/Program.cs
+++ 3.ExersiceHW1/ConsoleApp1/Program.cs
@@ -8,8 +8,19 @@
     {
         public static void Main()
         {
+            List<string> words = Console.ReadLine()
+                                        .Split(' ')
+                                        .ToList();
+
+            Console.WriteLine(String.Join(" ",words.OrderBy(x => x)));
+
+        }
+
+        public void LongImplementation()
+        {
             int a = 97;
             int z = 122;
+            char currentChar;
 
             IList<string> input = Console
                 .ReadLine()
@@ -20,7 +31,7 @@
 
             IList<char> listOfFirstLetter = new List<char>();
 
-                //TODO zashtita
+            //TODO zashtita
 
             foreach (var word in input)
             {
@@ -34,19 +45,20 @@
                 }
             }
 
-            for (int i = 0; i < listOfFirstLetter.Count; i++)
+            for (int i = 0; i < listOfFirstLetter.Count - 1; i++)
             {
+                //zashtita ot prepulvane
                 if (listOfFirstLetter[i] > listOfFirstLetter[i + 1])
                 {
-                    
-                }
-                else if(listOfFirstLetter[i] < listOfFirstLetter[i+1])
-                {
+                    currentChar = listOfFirstLetter[i + 1];
+                    listOfFirstLetter[i + 1] = listOfFirstLetter[i];
+                    listOfFirstLetter[i] = currentChar;
 
+                }
+                else if (listOfFirstLetter[i] == listOfFirstLetter[i + 1])
+                {
                 }
             }
-
-
         }
     }
 }
