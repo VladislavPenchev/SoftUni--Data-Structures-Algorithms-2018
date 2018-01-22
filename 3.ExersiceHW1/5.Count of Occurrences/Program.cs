f59586e8b745e498e3fe341415655ff4d0047dbd
VladislavPenchev <vgpenchev@gmail.com>
2018-01-22 23:21:27 +0200

    Start implementation of count of occurrences

---
+++ 3.ExersiceHW1/5.Count of Occurrences/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StartUpTemplate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            IList<int> input = Console
+                                .ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+            IList<int> result = SearchOccurrences(input);
+
+        }
+        
+        private static IList<int> SearchOccurrences(IList<int> list)
+        {
+            int currentOccurrence = 0;
+            int value = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[i] == list[j])
+                    {
+                        currentOccurrence++;
+                        value = list[i];
+                    }
+                }
+            }
+
+            IList<int> result = new List<int>();    
+            return result;
+        }
+    }
+}
