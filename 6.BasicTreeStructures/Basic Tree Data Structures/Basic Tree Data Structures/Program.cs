e793996bdea2fe96c9bd953c5329bb36d96acd91
VladislavPenchev <vgpenchev@gmail.com>
2018-01-29 11:24:03 +0200

    Implement basic tree datastructure read from console push in dictionary

---
+++ 6.BasicTreeStructures/Basic Tree Data Structures/Basic Tree Data Structures/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Basic_Tree_Data_Structures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tree<T>
+    {
+        public Tree(T value, Tree<T> parent)
+        {
+            this.Value = value;
+            this.Parent = parent;
+            this.Children = new List<Tree<T>>();
+        }
+
+        public T Value { get; set; }
+        public Tree<T> Parent { get; set;}
+        public List<Tree<T>> Children { get; set; }
+    }
+
+    public class Program
+    {
+        public static Dictionary<int, Tree<int>> tree = new Dictionary<int, Tree<int>>();
+
+        static void Main(string[] args)
+        {
+            int numberOfNodes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfNodes - 1; i++)
+            {
+                List<int> nodes = Console
+                                    .ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+                int parent = nodes[0];
+                int child = nodes[1];
+                if (!tree.ContainsKey(parent))
+                {
+                    tree.Add(parent,new Tree<int>(parent,null));
+                }
+                tree.Add(child, new Tree<int>(child, tree[parent]));
+                tree[parent].Children.Add(tree[child]);
+            }
+
+
+        }
+    }
+}
