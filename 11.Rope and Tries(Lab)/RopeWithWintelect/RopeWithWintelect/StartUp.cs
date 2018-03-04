18826421ebc32514619cb5b39efc22d25d4515d6
VladislavPenchev <vgpenchev@gmail.com>
2018-03-04 10:59:34 +0200

    Exam2017 1.LimitedMemory => full implement

---
+++ 11.Rope and Tries(Lab)/RopeWithWintelect/RopeWithWintelect/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace RopeWithWintelect
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+    using Wintellect.PowerCollections;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //int iterations = 1000000;
+            //Console.WriteLine($"Interations: {iterations}");
+            //BigList<string> rope = new BigList<string>();
+
+
+            //Stopwatch timer = new Stopwatch();
+            //timer.Start();
+            //for (int i = 0; i < iterations; i++)
+            //{
+            //    rope.Insert(0, "str");
+            //}
+            //Console.WriteLine($"Rope prepend time elapsed: {timer.ElapsedMilliseconds}");
+
+            //StringBuilder builder = new StringBuilder();
+
+            //timer = new Stopwatch();
+            //timer.Start();
+            //for (int i = 0; i < iterations; i++)
+            //{
+            //    builder.Insert(0, "str");
+            //}
+            //Console.WriteLine($"builder prepend time elapsed: {timer.ElapsedMilliseconds}");
+
+            StringBuilder builder = new StringBuilder();
+
+            var result = builder.Append("Gosho vv gosho");
+
+            Console.WriteLine(result);
+
+            var res = builder.Insert(1, "OKI");
+            Console.WriteLine(res);
+
+        }
+    }
+}
--- Exam2017_1.LimitedMemory/C#-Skeleton/LimitedMemory/LimitedMemoryCollection.cs
+++ Exam2017_1.LimitedMemory/C#-Skeleton/LimitedMemory/LimitedMemoryCollection.cs
@@ -1,41 +1,97 @@
 ﻿using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
+using System;
+using System.Collections;
 
 namespace LimitedMemory
 {
     public class LimitedMemoryCollection<K, V> : ILimitedMemoryCollection<K, V>
     {
-
-
+        private LinkedList<Pair<K, V>> _priority;
+        private Dictionary<K, LinkedListNode<Pair<K, V>>> _elements;
+        
 
         public LimitedMemoryCollection(int capacity)
         {
-            
+            this.Capacity = capacity;
+            this._priority = new LinkedList<Pair<K, V>>();
+            this._elements = new Dictionary<K, LinkedListNode<Pair<K, V>>>();
         } 
+
+        public int Capacity
+        {
+            get;
+            private set;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this._elements.Count;
+            }
+        }
+
+        public void Set(K key, V value)
+        {
+            if (!this._elements.ContainsKey(key))
+            {
+                if (this.Capacity <= this.Count)
+                {
+                    this.RemoveOldestElement();
+                }
+                this.AddElement(key,value);
+            }
+            else
+            {
+                var node = this._elements[key];
+                this._priority.Remove(node);
+                node.Value.Value = value;
+                this._priority.AddFirst(node);
+            }
+        }
+        
+        private void AddElement(K key,V value)
+        {
+            LinkedListNode<Pair<K, V>> node = new LinkedListNode<Pair<K, V>>(new Pair<K, V>(key,value));
+            this._elements.Add(key, node);
+            this._priority.AddFirst(node);
+
+        }
+
+        private void RemoveOldestElement()
+        {
+            var node = this._priority.Last;
+            this._elements.Remove(node.Value.Key);
+            this._priority.RemoveLast();
+        }
+
+        public V Get(K key)
+        {
+            if (!this._elements.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();
+            }
+
+            var node = this._elements[key];
+
+            this._priority.Remove(node);
+            this._priority.AddFirst(node);
+            return node.Value.Value;
+            
+        }
 
         public IEnumerator<Pair<K, V>> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            foreach (var node in this._priority)
+            {
+                yield return node;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
-        }
-
-        public int Capacity { get; private set; }
-
-        public int Count { get; private set; }
-
-        public void Set(K key, V value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public V Get(K key)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
